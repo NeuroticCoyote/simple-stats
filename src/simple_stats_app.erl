d023c215c8c8ea004fd155cce6616e94ae8f87c9
Joe Goodwin <josephgoodwin@hotmail.co.uk>
2021-07-26 15:19:08 +0100

    send stats via tcp to logstash

--- src/simple_stats_app.erl
+++ src/simple_stats_app.erl
@@ -7,90 +7,17 @@
 
 -behaviour(application).
 
--export([start/2, stop/1, receiver/1]).
--export([update_gauge/2, update_spiral/2, update_histogram/2, update_stat/3, get_stat/1]).
-
--define(INCREMENT_HTTP_RECEIVED_GET_COUNTER(Integer), update_spiral([http, received, get], Integer)).
+-export([start/2, stop/1]).
 
 %% ======================================================
 %% API
 %% ======================================================
 
 start(_StartType, _StartArgs) ->
+    lager:start(),
     application:ensure_all_started(exometer_core),
-    {ok, ListenSocket} = gen_tcp:listen(1717, [{reuseaddr, true}, binary]),
-    spawn(simple_stats_app, receiver, [ListenSocket]),
+    http_handler:start_http_handler(),
     simple_stats_sup:start_link().
 
 stop(_State) ->
     ok.
-
-update_gauge(Name, Integer) ->
-    update_stat(Name, gauge, Integer).
-
-update_spiral(Name, Integer) ->
-    update_stat(Name, spiral, Integer).
-
-update_histogram(Name, Integer) ->
-    update_stat(Name, histogram, Integer).
-
-%% name needs a list such as [counter, name, here]
-%% type can be: gauge, meter, spiral, histogram etc
-%% e.g update_counter([basic, counter], gauge, 100)
-update_stat(Name, Type, Value) ->
-    case exometer:update(Name, Value) of
-        {error, not_found} ->
-            exometer:new(Name, Type),
-            exometer:update(Name, Value),
-            Stat = get_stat(Name),
-            stats:update_stats({Name, Stat});
-        _ ->
-            Stat = get_stat(Name),
-            stats:update_stats({Name, Stat})
-    end.
-
-get_stat(Name) ->
-    DataPoints = exometer:info(Name, datapoints),
-    exometer:get_value(Name, DataPoints).
-
-receiver(ListenSocket) ->
-    {ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
-    ok = inet:setopts(AcceptSocket, [{packet, http_bin}]),
-
-    receive
-        {http, _, _Msg} ->
-            ?INCREMENT_HTTP_RECEIVED_GET_COUNTER(1),
-            BinaryResult = maps:fold(
-                fun(K,V, Acc) ->
-                    BinaryKey = convert_key(K),
-                    BinaryValues = convert_value(V, BinaryKey, <<>>),
-                    <<Acc/binary, BinaryValues/binary >>
-                end, <<>>, stats:get_stats()),
-
-            ok = inet:setopts(AcceptSocket, [{packet, raw}]),
-            ok = gen_tcp:send(AcceptSocket, reply(BinaryResult)),
-            gen_tcp:close(AcceptSocket);
-        _Other ->
-            ok
-    end,
-    receiver(ListenSocket).
-
-convert_key(Key) when is_atom(Key) ->
-    atom_to_binary(Key, latin1);
-convert_key(Key) when is_integer(Key) ->
-    integer_to_binary(Key);
-convert_key(Key) when is_tuple(Key) ->
-    List = tuple_to_list(Key),
-    lists:foldl(fun(V, Acc) -> << Acc/binary, (atom_to_binary(V, latin1))/binary , ".">> end, <<>>, List).
-
-convert_value([], _, Acc) ->
-    Acc;
-convert_value([{Key,Value} | Rest], BinaryKey, Acc) ->
-    BinaryKey2 = convert_key(Key),
-    BinaryValue = convert_key(Value),
-    convert_value(Rest, BinaryKey, << Acc/binary, BinaryKey/binary, BinaryKey2/binary, "=", BinaryValue/binary, "\n" >>).
-
-reply(Result) ->
-    <<"HTTP/1.0 200 OK\r\n",
-    "Content-Length: ", (integer_to_binary(byte_size(Result)))/binary, "\r\n\r\n", Result/binary,
-    "\n">>.
--- src/stats.erl
+++ src/stats.erl
@@ -10,6 +10,7 @@
 -author("joegoodwin").
 
 -behaviour(gen_server).
+-compile([{parse_transform, lager_transform}]).
 
 %% API
 -export([start_link/0, update_stats/1, get_stats/0]).
@@ -19,8 +20,9 @@
 	code_change/3]).
 
 -define(SERVER, ?MODULE).
+-define(INCREMENT_SCHEDULED_REPORT_COUNT(), simple_stats:update_counter([scheduled, reports], 1)).
 
--record(stats, {stats=#{}}).
+-record(stats, {stats=#{}, tcp_socket}).
 
 %%%===================================================================
 %%% API
@@ -40,8 +42,12 @@ get_stats() ->
 %%%===================================================================
 
 init([]) ->
-	{ok, #stats{}}.
+	UpdatedState = connect_tcp(),
+	schedule_report(),
+	{ok, UpdatedState}.
 
+handle_call({update_state, State}, _From, _State) ->
+	{reply, ok, State};
 handle_call(get_stats, _From, State = #stats{stats = StatsMap}) ->
 	{reply, StatsMap, State};
 handle_call(_Request, _From, State = #stats{}) ->
@@ -53,6 +59,10 @@ handle_cast({update_stats, {StatKey, {ok, Stat}}}, State = #stats{stats = StatsM
 handle_cast(_Request, State = #stats{}) ->
 	{noreply, State}.
 
+handle_info(report, State) ->
+ 	format_and_send_state(State),
+	schedule_report(),
+	{noreply, State};
 handle_info(_Info, State = #stats{}) ->
 	{noreply, State}.
 
@@ -61,3 +71,35 @@ terminate(_Reason, _State = #stats{}) ->
 
 code_change(_OldVsn, State = #stats{}, _Extra) ->
 	{ok, State}.
+
+connect_tcp() ->
+	case gen_tcp:connect(localhost, 10001, [binary, {packet, 0}, {active, false}]) of
+		{ok, Socket} ->
+			lager:info("Connected to localhost on port 10001..."),
+			#stats{tcp_socket = Socket};
+		{error, _} = Error ->
+			lager:error("Error connecting to localhost, retrying in 2 seconds. Error: ~p~n", [Error]),
+			timer:sleep(2000),
+			connect_tcp()
+	end.
+
+schedule_report() ->
+	?INCREMENT_SCHEDULED_REPORT_COUNT(),
+	erlang:send_after(10000, self(), report).
+
+format_and_send_state(#stats.stats = {}) ->
+	ok;
+format_and_send_state(#stats.tcp_socket = undefined) ->
+	ok;
+format_and_send_state(#stats{stats = Stats, tcp_socket = Socket}) ->
+	lager:info("Formatting and sending report to logstash on port 10002..."),
+	FormattedStats =
+		maps:fold(fun(K,V,AccIn) ->
+			lists:foldl(fun({K1,V1}, Acc) ->
+					FormattedKey = lists:foldr(fun(E2, Acc2) -> <<(atom_to_binary(E2))/binary, ".",  Acc2/binary>> end, <<"">>, tuple_to_list(K)),
+					FormattedKey1 = <<FormattedKey/binary, (atom_to_binary(K1))/binary>>,
+					maps:put(FormattedKey1, V1, Acc)
+				end, AccIn, V)
+			end, #{}, Stats),
+	JSON = jsx:encode(FormattedStats),
+	ok = gen_tcp:send(Socket, <<JSON/binary, "\n">>).
